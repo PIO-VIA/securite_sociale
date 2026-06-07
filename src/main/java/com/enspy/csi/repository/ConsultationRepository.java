6152f71cfebf376c75c9a24aaabfddc839c7232e
Erica Donjo <ericadonjo2006@gmail.com>
2026-06-07 17:04:19 +0100

    src/main/java/com/enspy/csi/repository/PrescriptionRepository.java

--- src/main/java/com/enspy/csi/repository/ConsultationRepository.java
+++ src/main/java/com/enspy/csi/repository/ConsultationRepository.java
@@ -4,6 +4,12 @@ import com.enspy.csi.entity.Consultation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+    List<Consultation> findByAssureId(Long assureId);
+    List<Consultation> findByGeneralisteId(Long generalisteId);
+    List<Consultation> findByAssureIdAndGeneralisteId(Long assureId, Long generalisteId);
+    Long countByAssureId(Long assureId);
 }
